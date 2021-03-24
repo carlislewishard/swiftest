77df212b695adfae37d1f075480e0ab03f0a4b41
David Minton <daminton@purdue.edu>
2021-03-23 20:20:31 -0400

    Removed debugging cruft

--- src/symba/symba_frag_pos.f90
+++ src/symba/symba_frag_pos.f90
@@ -19,7 +19,7 @@ subroutine symba_frag_pos (mtot, m1, m2, rhill, x, v, m_frag, x_frag, v_frag)
    integer(I4B)                            :: i, j, nfrag
    real(DP), dimension(NDIM)               :: v_cross_x, delta_v, delta_x
    real(DP)                                :: phase_ang, theta, v_frag_norm, r_frag_norm, v_col_norm, r_col_norm
-   real(DP)                                :: f_anelastic, Etot_before, KE_before, U_before, U_after, KE_after, v1mag2, v2mag2
+   real(DP)                                :: f_anelastic, Etot_before, KE_before, U_before, v1mag2, v2mag2
    real(DP), dimension(NDIM)               :: v_col_unit_vec, tri_pro, tri_pro_unit_vec, v_com, vc1, vc2
    integer(I4B), save                      :: thetashift = 0
    integer(I4B), parameter                 :: SHIFTMAX = 9
@@ -63,8 +63,6 @@ subroutine symba_frag_pos (mtot, m1, m2, rhill, x, v, m_frag, x_frag, v_frag)
    KE_before = 0.5_DP * (m1 * v1mag2 + m2 * v2mag2)
    U_before = (m1 * m2) / r_col_norm
    Etot_before = KE_before + U_before
-   KE_after = 0.0_DP
-   U_after = 0.0_DP
    ! Calculate the position and velocity of each fragment 
    do i=1, nfrag 
       v_frag_norm = sqrt(2 * f_anelastic * Etot_before / (nfrag * m_frag(i))) 
@@ -75,24 +73,6 @@ subroutine symba_frag_pos (mtot, m1, m2, rhill, x, v, m_frag, x_frag, v_frag)
       x_frag(:,i) =  r_frag_norm * ((cos(phase_ang + theta * i)) * v_col_unit_vec(:)  + &
                                  (sin(phase_ang + theta * i)) * tri_pro_unit_vec(:))
 
-      !KE_after = KE_after + 0.5_DP * m_frag(i) * dot_product(v_frag(:,i), v_frag(:,i))
    end do
-   !do i = 1, nfrag
-   !   do j = i+1, nfrag
-   !      delta_x(:) = x_frag(:,i) - x_frag(:,j)
-   !      U_after = U_after + m_frag(i) * m_frag(j) / norm2(delta_x(:))
-   !   end do
-   !end do 
-
-   !write(*,*) "SYMBA_FRAG_POS KE_before : ", KE_before
-   !write(*,*) "SYMBA_FRAG_POS KE_after : ", KE_after
-   !write(*,*) "SYMBA_FRAG_POS KE_ratio : ", KE_after / KE_before
-   !write(*,*) "SYMBA_FRAG_POS U_before : ", U_before
-   !write(*,*) "SYMBA_FRAG_POS U_after : ", U_after
-   !write(*,*) "SYMBA_FRAG_POS U_ratio : ", U_after / U_before
-   !write(*,*) "SYMBA_FRAG_POS E_before : ", KE_before + U_before
-   !write(*,*) "SYMBA_FRAG_POS E_after : ", KE_after + U_after
-   !write(*,*) "SYMBA_FRAG_POS E_ratio : ", (KE_after + U_after) / (KE_before + U_before)
-!
    return 
 end subroutine symba_frag_pos
