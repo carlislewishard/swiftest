591ad0d64acf5504b42aeb65248466052b09f1e7
Carlisle Wishard <cwishard@purdue.edu>
2021-03-30 15:34:26 -0400

    re-did calculation for collisional x and v and saved to plplenc_list

--- src/symba/symba_merge_pl.f90
+++ src/symba/symba_merge_pl.f90
@@ -23,17 +23,21 @@ subroutine symba_merge_pl(t, dt, index_enc, nmergesub, mergesub_list, npl, symba
    type(symba_pl), intent(inout)              :: symba_plA
    type(user_input_parameters), intent(inout) :: param
 
-   integer(I4B)                            :: i, j, k, index_parent, index_child, p1, p2
-   integer(I4B)                            :: nchild_inherit, nchild_orig, nchild_new
-   real(DP), dimension(NDIM)               :: vbs
-   integer(I4B), dimension(2)              :: idx, name
-   real(DP), dimension(2)                  :: radius, mass 
-   real(DP), dimension(NDIM, 2)            :: x, v
-   real(DP)                                :: r2, rlim, rlim2, vdotr, tcr2, dt2, a, e, q
-   real(DP)                                :: mtot
-   real(DP), dimension(NDIM)               :: xr, vr
-   logical                                 :: lcollision
-   integer(I4B), dimension(:), allocatable :: temp
+   integer(I4B)                               :: i, j, k, index_parent, index_child, p1, p2
+   integer(I4B)                               :: nchild_inherit, nchild_orig, nchild_new
+   real(DP), dimension(NDIM)                  :: vbs
+   integer(I4B), dimension(2)                 :: idx, name
+   real(DP), dimension(2)                     :: radius, mass 
+   real(DP), dimension(NDIM, 2)               :: x, v
+   real(DP)                                   :: r2, rlim, rlim2, vdotr, tcr2, dt2, a, e, q
+   real(DP)                                   :: mtot
+   real(DP), dimension(NDIM)                  :: xr, vr
+   logical                                    :: lcollision
+   integer(I4B), dimension(:), allocatable    :: temp
+   real(DP)                                   :: n, a1, a2, r1, r2, f1, f2, x1, x2, y1, y2
+   real(DP)                                   :: m1, m2, mtot, vx1, vx2, vy1, vy2
+   real(DP), dimension(NDIM)                  :: xh1, xh2, vb1, vb2, vbcom, xhcom
+   real(DP), dimension(NDIM)                  :: v1_wrt2, v1, v2, v1_unit_vec, v2_unit_vec, p1, p2
 
    ! recalculates vbs 
    call coord_vb2vh(npl, symba_plA%helio%swiftest)
@@ -42,26 +46,97 @@ subroutine symba_merge_pl(t, dt, index_enc, nmergesub, mergesub_list, npl, symba
    idx(1) = plplenc_list%index1(index_enc)
    idx(2) = plplenc_list%index2(index_enc)
 
+   m1 = symba_plA%helio%swiftest%mass(idx(1))         ! mass of larger parent
+   m2 = symba_plA%helio%swiftest%mass(idx(2))         ! mass of smaller parent
+   xh1(:) = symba_plA%helio%swiftest%xh(idx(1))       ! heliocentric position of larger parent at time collision is detected / deemed inevitable
+   xh2(:) = symba_plA%helio%swiftest%xh(idx(2))       ! heliocentric position of smaller parent at time collision is detected / deemed inevitable
+   vb1(:) = symba_plA%helio%swiftest%vb(idx(1))       ! barycentric velocity of larger parent at time collision is detected / deemed inevitable
+   vb2(:) = symba_plA%helio%swiftest%vb(idx(2))       ! barycentric velocity of smaller parent at time collision is detected / deemed inevitable
+
+   vbcom(:) = (m1 * vb1 + m2 * vb2) / (m1 + m2)
+   xhcom(:) = (m1 * xh1 + m2 * xh2) / (m1 + m2)
+   mtot = m1 + m2
+
    rlim = symba_plA%helio%swiftest%radius(idx(1)) + symba_plA%helio%swiftest%radius(idx(2))
    xr(:) = symba_plA%helio%swiftest%xh(:, idx(2)) - symba_plA%helio%swiftest%xh(:, idx(1))
    r2 = dot_product(xr(:), xr(:))
    rlim2 = rlim**2
+   vr(:) = symba_plA%helio%swiftest%vb(:, idx(2)) - symba_plA%helio%swiftest%vb(:, idx(1))
+   vdotr = dot_product(xr(:), vr(:))
 
    lcollision = .false.
    ! checks if bodies are actively colliding in this time step
    if (r2 <= rlim2) then 
       lcollision = .true.
+
+      !! The velocity of body 1 with respect to body 2
+      v1_wrt2 = sqrt(vb1**2 - (m1 * m2 / sqrt(r2)) + (m1 * m2 / rlim))                    !! From conservation of energy
+      !! The velocity of body 2 with respect to the barycenter of the collision
+      v2 = ((m1 * vb1) + (m2 * vb2) - (m1 * v1_wrt2)) / (m1 + m2)                         !! From conservation of linear momentum
+      !! The velocity of body 1 with respect to the barycenter of the collision
+      v1 = v1_wrt2 + v2
+
+      !! The position of body 1 with respect to the barycenter of the collision
+      v1_unit_vec = - v1 / norm2(v1)                                                      !! The opposite trajectory of body 1 
+      p1 = xhcom * (m2 / mtot) * v1_unit_vec
+      plplenc_list%x1(1,index_enc) = p1(1) + xhcom(1)
+      plplenc_list%x1(2,index_enc) = p1(2) + xhcom(2)
+      plplenc_list%x1(3,index_enc) = xh1(3)
+      plplenc_list%v1(1,index_enc) = v1(1) + vbcom(1)
+      plplenc_list%v1(2,index_enc) = v1(2) + vbcom(2)
+      plplenc_list%v1(3,index_enc) = vb1(3)
+
+      !! The position of body 2 with respect to the barycenter of the collision
+      v2_unit_vec = - v2 / norm2(v2)                                                      !! The opposite trajectory of body 2
+      p2 = xhcom * (m1 / mtot) * v2_unit_vec
+      plplenc_list%x2(1,index_enc) = p2(1) + xhcom(1)
+      plplenc_list%x2(2,index_enc) = p2(2) + xhcom(2)
+      plplenc_list%x2(3,index_enc) = xh2(3)
+      plplenc_list%v2(1,index_enc) = v2(1) + vbcom(1)
+      plplenc_list%v2(2,index_enc) = v2(2) + vbcom(2)
+      plplenc_list%v2(3,index_enc) = vb2(3)
+
       ! if they are not actively colliding in  this time step, checks if they are going to collide next time step based on velocities and q
    else 
-      vr(:) = symba_plA%helio%swiftest%vb(:, idx(2)) - symba_plA%helio%swiftest%vb(:, idx(1))
-      vdotr = dot_product(xr(:), vr(:))
       if (plplenc_list%lvdotr(index_enc) .and. (vdotr > 0.0_DP)) then 
          tcr2 = r2 / dot_product(vr(:), vr(:))
          dt2 = dt**2
          if (tcr2 <= dt2) then
-            mtot = symba_plA%helio%swiftest%mass(idx(1)) + symba_plA%helio%swiftest%mass(idx(2))
             call orbel_xv2aeq(xr(:), vr(:), mtot, a, e, q)
-            if (q < rlim) lcollision = .true.
+            if (q < rlim) then 
+               lcollision = .true.
+
+               n = sqrt(mtot / (a**3))                                                    !! Eq. 2.26 Murray & Dermott
+
+               !! The orbit of body 1 with respect to the barycenter of the collision
+               a1 = a * (m2 / mtot)                                                       !! Eq. 2.109 Murray & Dermott
+               r1 = rlim * (m2 / mtot)                                                    !! Eq. 2.109 Murray & Dermott
+               f1 = acos(((a1 * (1.0_DP - e**2)) / (r1 * e)) - (1.0_DP / e))              !! Eq. 2.20 Murray & Dermott
+               x1 = r1 * cos(f1)                                                          !! Eq. 2.21 Murray & Dermott
+               y1 = r1 * sin(f1)                                                          !! Eq. 2.21 Murray & Dermott
+               vx1 = = - ((n * a1) / sqrt(1 - e**2)) * sin(f1)                            !! Eq. 2.36 Murray & Dermott
+               vy1 = ((n * a1) / sqrt(1 - e**2)) * (e + cos(f1))                          !! Eq. 2.36 Murray & Dermott
+               plplenc_list%x1(1,index_enc) = x1 + xhcom(1)
+               plplenc_list%x1(2,index_enc) = y1 + xhcom(2)
+               plplenc_list%x1(3,index_enc) = xh1(3)
+               plplenc_list%v1(1,index_enc) = vx1 + vbcom(1)
+               plplenc_list%v1(2,index_enc) = vy1 + vbcom(2)
+               plplenc_list%v1(3,index_enc) = vb1(3)
+
+               !! The orbit of body 2 with respect to the barycenter of the collision
+               a2 = a * (m1 / mtot)                                                       !! Eq. 2.109 Murray & Dermott
+               r2 = rlim * (m1 / mtot)                                                    !! Eq. 2.109 Murray & Dermott
+               f2 = acos(((a2 * (1.0_DP - e**2)) / (r2 * e)) - (1.0_DP / e))              !! Eq. 2.20 Murray & Dermott
+               x2 = r2 * cos(f2)                                                          !! Eq. 2.21 Murray & Dermott
+               y2 = r2 * sin(f2)                                                          !! Eq. 2.21 Murray & Dermott
+               vx2 = = - ((n * a2) / sqrt(1 - e**2)) * sin(f2)                            !! Eq. 2.36 Murray & Dermott
+               vy2 = ((n * a2) / sqrt(1 - e**2)) * (e + cos(f2))                          !! Eq. 2.36 Murray & Dermott
+               plplenc_list%x2(1,index_enc) = x2 + xhcom(1)
+               plplenc_list%x2(2,index_enc) = y2 + xhcom(2)
+               plplenc_list%x2(3,index_enc) = xh2(3)
+               plplenc_list%v2(1,index_enc) = vx2 + vbcom(1)
+               plplenc_list%v2(2,index_enc) = vy2 + vbcom(2)
+               plplenc_list%v2(3,index_enc) = vb2(3)
          end if
 
       end if
