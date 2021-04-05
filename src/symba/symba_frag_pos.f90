b2d3d16647adc9b4841d2ffebfbca548c3b525f4
David Minton <daminton@purdue.edu>
2021-04-05 11:18:29 -0400

    Restructured energy calculations. Still not quite correct and now angular momentum is not conserved

--- src/symba/symba_frag_pos.f90
+++ src/symba/symba_frag_pos.f90
@@ -19,206 +19,164 @@ subroutine symba_frag_pos (symba_plA, idx_parents, x, v, L_spin, Ip, mass, radiu
    type(symba_pl)                            :: symba_pla
 
    real(DP), dimension(NDIM, 2)            :: rot
-   integer(I4B)                            :: i, j, nfrag, fam_size
+   integer(I4B)                            :: i, j, nfrag, fam_size, istart, non_fam_size
    real(DP), dimension(NDIM)               :: v_cross_x, delta_v, delta_x, xcom, vcom
    real(DP)                                :: mtot, phase_ang, theta, v_frag_norm, r_frag_norm, v_col_norm, r_col_norm
    real(DP)                                :: f_anelastic, Etot_before, KE_before, U_before, v1mag2, v2mag2, U_p1_before, U_p2_before 
    real(DP)                                :: U_after, KE_spin_before, KE_spin_after, KE_after, KE_corrected, U_corrected, U_frag_after
    real(DP), dimension(NDIM)               :: v_col_unit_vec, tri_pro, tri_pro_unit_vec, v_com, vc1, vc2
-   real(DP), dimension(NDIM)               :: xh_family, xb_family, vb_family, rot_family, xh_other, vb_other, Ltot, h
-   real(DP)                                :: m_family, rad_family, m_other, rot2, v2, ip_family
+   real(DP), dimension(NDIM)               :: Ltot, h
+   real(DP)                                :: rot2, v2, ip_family
    integer(I4B), save                      :: thetashift = 0
    integer(I4B), parameter                 :: SHIFTMAX = 9
-   real(DP), dimension(2)                  :: m
-   integer(I4B), dimension(:), allocatable :: family
+   integer(I4B), dimension(:), allocatable :: family, non_family
 
-   fam_size = 2 + symba_plA%kin(idx_parents(1))%nchild + symba_plA%kin(idx_parents(2))%nchild
-   allocate(family(fam_size))
-   family(1) = idx_parents(1)
-   family(2) = idx_parents(2)
+   associate(nchild1 => symba_plA%kin(idx_parents(1))%nchild, nchild2 => symba_plA%kin(idx_parents(2))%nchild, &
+             xhpl => symba_plA%helio%swiftest%xh, xbpl => symba_plA%helio%swiftest%xh, vbpl => symba_plA%helio%swiftest%vb, &
+             Mpl => symba_plA%helio%swiftest%mass, Ippl => symba_plA%helio%swiftest%Ip, radpl => symba_plA%helio%swiftest%radius, &
+             rotpl => symba_plA%helio%swiftest%rot, status => symba_plA%helio%swiftest%status)
+      ! Make the list of family members (bodies involved in the collision)
+      fam_size = 2 + nchild1 + nchild2
+      allocate(family(fam_size))
+      family(1) = idx_parents(1)
+      family(2) = idx_parents(2)
+      istart = 2 + nchild1
 
-   if (symba_plA%kin(idx_parents(1))%nchild > 0) then
-      family(3:symba_plA%kin(idx_parents(1))%nchild) = symba_plA%kin(idx_parents(1))%child(:)
-      if (symba_plA%kin(idx_parents(2))%nchild > 0) then
-         family(symba_plA%kin(idx_parents(1))%nchild + 1:symba_plA%kin(idx_parents(2))%nchild) = symba_plA%kin(idx_parents(2))%child(:)
-      end if 
-   else if (symba_plA%kin(idx_parents(2))%nchild > 0) then
-      family(3:symba_plA%kin(idx_parents(2))%nchild) = symba_plA%kin(idx_parents(2))%child(:)
-   end if 
+      if (nchild1 > 0) family(3:istart) = symba_plA%kin(idx_parents(1))%child(:)
+      if (nchild2 > 0) family(istart+1:istart+1+nchild2) = symba_plA%kin(idx_parents(2))%child(:)
 
-   nfrag = size(x_frag, 2)
-   mtot = sum(mass(:))
-   xcom(:) = (mass(1) * x(:,1) + mass(2) * x(:,2)) / mtot
-   vcom(:) = (mass(1) * v(:,1) + mass(2) * v(:,2)) / mtot 
-
-   m(:) = symba_plA%helio%swiftest%mass(idx_parents(:)) ! just the parents, not including their children
-
-   U_before = 0.0_DP
-   KE_before = 0.0_DP
-   Ltot = 0.0_DP
-
-   do i = 1, size(family)
-      !! Set the mass, position, and velocity of the body in the family
-      m_family   = symba_plA%helio%swiftest%mass(family(i))
-      xh_family  = symba_plA%helio%swiftest%xh(:,family(i))
-      xb_family  = symba_plA%helio%swiftest%xb(:,family(i))
-      vb_family  = symba_plA%helio%swiftest%vb(:,family(i))
-      ip_family  = symba_plA%helio%swiftest%ip(3,family(i))
-      rad_family = symba_plA%helio%swiftest%radius(family(i))
-      rot_family = symba_plA%helio%swiftest%rot(:,family(i))
-
-      do j = 1, size(symba_plA%helio%swiftest%mass(:))
-         !! Pluck out two bodies, one from the family array and one from symba_plA
-         !! If the body in symba_plA is the same as the body in the family array, skip it
-         if (symba_plA%helio%swiftest%name(j) == symba_plA%helio%swiftest%name(family(i))) cycle
-
-         !! Set the mass, position, and velocity of the body in the system
-         m_other = symba_plA%helio%swiftest%mass(j)
-         xh_other = symba_plA%helio%swiftest%xh(:,j)
-         vb_other = symba_plA%helio%swiftest%vb(:,j)
-
-         !! If the body in symba_plA is parent 1, then set the mass to be of parent 1 without its children
-         if (symba_plA%helio%swiftest%name(j) == symba_plA%helio%swiftest%name(idx_parents(1))) m_other = m(1)
-         !! If the body in symba_plA is parent 2, then set the mass to be of parent 2 without its children
-         if (symba_plA%helio%swiftest%name(j) == symba_plA%helio%swiftest%name(idx_parents(1))) m_other = m(2)
-         !! If the body in family is parent 1, then set the mass to be of parent 1 without its children
-         if (symba_plA%helio%swiftest%name(family(1)) == symba_plA%helio%swiftest%name(idx_parents(1))) m_family = m(1)
-         !! If the body in family is parent 2, then set the mass to be of parent 2 without its children
-         if (symba_plA%helio%swiftest%name(family(2)) == symba_plA%helio%swiftest%name(idx_parents(2))) m_family = m(2)
-
-         !! Calculate the potential energy between the two bodies
-         U_before = U_before - (m_family * m_other / norm2(xh_family - xh_other))
-      end do 
-
-      !! Find the angular momentum 
-      v2   = dot_product(vb_family, vb_family)
-      rot2 = dot_product(rot_family, rot_family)
-      h(1) = xb_family(2) * vb_family(3) - xb_family(3) * vb_family(2)
-      h(2) = xb_family(3) * vb_family(1) - xb_family(1) * vb_family(3)
-      h(3) = xb_family(1) * vb_family(2) - xb_family(2) * vb_family(1)
-      
-      !! Calculate the orbital and rotational kinetic energy between the two bodies 
-      Ltot(:) = Ltot(:) + m_family * (h(:) + ip_family * rad_family**2 * rot_family)
-      KE_before = KE_before + 0.5_DP * m_family * (v2 + ip_family * rot2 * rad_family**2)
-   end do
-
-   Etot_before = KE_before + U_before
-
-   ! Calculate the position of each fragment 
-   ! Theta is a phase shift value that ensures that successive nearby collisions in a single step are rotated to avoid possible overlap
-   theta = (2 * PI) / nfrag
-   ! Shifts the starting circle of fragments around so that multiple fragments generated 
-   ! from a single collision in a single time step don't pile up on top of each other
-   phase_ang = theta * thetashift / SHIFTMAX
-   thetashift = thetashift + 1
-   IF (thetashift >= shiftmax) thetashift = 0
-
-   ! Calculate the triple product to get the plane of the fragment distribution
-   delta_v(:) = v(:, 2) - v(:, 1)
-   delta_x(:) = x(:, 2) - x(:, 1)
-   call util_crossproduct(delta_v,delta_x,v_cross_x)
-   call util_crossproduct(v_cross_x,delta_v,tri_pro)
-   tri_pro_unit_vec(:) = tri_pro(:) / norm2(tri_pro(:))
-   v_col_norm = norm2(delta_v(:))               ! pre-collision velocity magnitude
-   r_col_norm = norm2(delta_x(:))               ! pre-collision distance 
-   v_col_unit_vec(:) = delta_v(:) / v_col_norm  ! unit vector of collision velocity 
-
-   do i = 1, nfrag
-      ! Place the fragments on the collision plane at a distance proportional to mass wrt the collisional barycenter
-      ! This gets updated later after the new potential energy is calculated
-      r_frag_norm = r_col_norm * mtot / m_frag(i)
-
-      x_frag(:,i) =  r_frag_norm * ((cos(phase_ang + theta * i)) * v_col_unit_vec(:)  + &
-                                    (sin(phase_ang + theta * i)) * tri_pro_unit_vec(:))                 
-      ! Apply a simple mass weighting first to ensure that the velocity follows the barycenter
-      ! This gets updated later after the new potential and kinetic energy is calcualted
-      v_frag_norm = v_col_norm * mtot / m_frag(i)   
-      v_frag(:,i) =  v_frag_norm * ((cos(phase_ang + theta * i)) * v_col_unit_vec(:) + &
-                                    (sin(phase_ang + theta * i)) * tri_pro_unit_vec(:))                                 
-
-   end do
-
-   U_after = 0.0_DP
-   ! Calculate the new potential energy of the system of fragments on each other
-   do i = 1, nfrag
-      do j = 1, size(symba_plA%helio%swiftest%mass(:))
-         !! Skip a body in symba_plA if it is one of the parent bodies
-         if ((symba_plA%helio%swiftest%name(j) == symba_plA%helio%swiftest%name(idx_parents(1))) &
-            .or. (symba_plA%helio%swiftest%name(j) == symba_plA%helio%swiftest%name(idx_parents(1)))) cycle
-         !! Pluck out two bodies, one from the fragment array and one from symba_plA
-         !! Set the mass, position, and velocity of the body in the system
-         m_other = symba_plA%helio%swiftest%mass(j)
-         xh_other = symba_plA%helio%swiftest%xh(:,j)
-         !! Calculate the potential energy between the two bodies
-         U_after = U_after - (m_frag(i) * m_other / norm2((x_frag(:,i) + xcom(:)) - xh_other))
-      end do 
-
-      do j = 1, nfrag
-         !! If we have selected the same fragment in both the inner loop and the outer loop, skip it
-         if (i == j) cycle
-         !! Calculate the potential energy between the two bodies
-         U_after = U_after - m_frag(i) * m_frag(j) / norm2(x_frag(:,i) - x_frag(:,j))
+      ! Make the list of non-family members (bodies not involved in the collision)
+      non_fam_size = count(status(:) == ACTIVE) - fam_size
+      allocate(non_family(non_fam_size))
+      i = 0
+      do j = 1, non_fam_size
+         if (any(family(:) == j)) cycle
+         i = i + 1
+         non_family(i) = j
       end do
-   end do
 
-   ! Adjust fragment positions so that they have the same potential energy as the original collisional pair
-   x_frag(:,:) = x_frag(:,:) * U_after / U_before
-   U_after = U_before
+      U_before = 0.0_DP
+      KE_before = 0.0_DP
+      Ltot = 0.0_DP
 
-   ! Adjust fragment positiosn so that their center of mass follows the original pre-impact trajectory
-   call symba_frag_adjust (xcom, vcom, x, v, mass, radius, L_spin, Ip_frag, m_frag, rad_frag, x_frag, v_frag, rot_frag)
-   
-   ! Compute the current kinetic energy of the system so we can scale the velocity vectors to the correct value
-   KE_after = 0.0_DP
-   KE_spin_after = 0.0_DP
-   do i = 1, nfrag
-      KE_after = KE_after + 0.5_DP * m_frag(i) * dot_product(v_frag(:,i), v_frag(:,i))
-      KE_spin_after = 0.5_DP * m_frag(i) * rad_frag(i)**2 * Ip_frag(3,i) * dot_product(rot_frag(:,i), rot_frag(:,i))
-   end do
+      do i = 1, fam_size
+         ! Calculate the potential energy between family members
+         do j = i + 1, fam_size
+            U_before = U_before - Mpl(family(i)) * Mpl(family(j)) / norm2(xhpl(:,family(i)) - xhpl(:,family(j)))
+         end do
+         
+         ! Add the contribution due to non-family members
+         do j = 1, non_fam_size
+            U_before = U_before - Mpl(family(i)) * Mpl(non_family(j)) / norm2(xhpl(:,family(i)) - xhpl(:,non_family(j)))
+         end do
 
-   ! Adjust the fragment velocities so that they have the their total energy reduced by an amount set by the anelastic parameter
-   ! Make sure we don't end up with negative energy (bound system). If so, we'll adjust the radius so that the potential energy
-   ! takes up the negative part
-   f_anelastic = 0.1_DP ! TODO: Should this be set by the user or kept as a constant?
-   KE_corrected = f_anelastic * Etot_before - KE_spin_after - U_after
-   if (KE_corrected < 0.0_DP) then
-      U_corrected = U_after + KE_corrected
-      x_frag(:,:) = x_frag(:,:) * U_corrected / U_after
-      U_after = U_corrected
-      KE_corrected = 0.0_DP
-   end if
+         !! Calculate the orbital and spin kinetic energy
+         v2   = dot_product(vbpl(:,family(i)), vbpl(:,family(i)))
+         rot2 = dot_product(rotpl(:,family(i)), rotpl(:,family(i)))
+         call util_crossproduct(xbpl(:,family(i)), vbpl(:,family(i)), h(:))
+         
+         !! Calculate the orbital and rotational kinetic energy between the two bodies 
+         Ltot(:) = Ltot(:) + Mpl(family(i)) * (h(:) + Ippl(3, family(i)) * radpl(family(i))**2 * rotpl(:,family(i)))
+         KE_before = KE_before + 0.5_DP * Mpl(family(i)) * (v2 + Ippl(3,family(i)) * rot2 * radpl(family(i))*2)
+      end do
 
-   v_frag(:,:) = v_frag(:,:) * sqrt(KE_corrected / KE_after)
+      Etot_before = KE_before + U_before
 
-   ! REMOVE THE FOLLOWING AFTER TESTING
-   ! Calculate the new energy of the system of fragments
-   KE_after = 0.0_DP
-   do i = 1, nfrag
-      KE_after = KE_after + 0.5_DP * m_frag(i) * dot_product(v_frag(:,i), v_frag(:,i))
-   end do
-   U_after = 0.0_DP
-   do j = 1, nfrag - 1
-      do i = j+1, nfrag
-         delta_x(:) = x_frag(:,i) - x_frag(:,j)
-         U_after = U_after - m_frag(i) * m_frag(j) / norm2(delta_x(:))
-      end do 
-   end do
+      ! Now create the fragment distribution
+      nfrag = size(x_frag, 2)
+      mtot = sum(mass(:))
+      ! Find the center of mass of the collisional system
+      xcom(:) = (mass(1) * x(:,1) + mass(2) * x(:,2)) / mtot
+      vcom(:) = (mass(1) * v(:,1) + mass(2) * v(:,2)) / mtot 
 
-   write(*,*) "SYMBA_FRAG_POS KE_before : ", KE_before + KE_spin_before
-   write(*,*) "SYMBA_FRAG_POS KE_after  : ", KE_after + KE_spin_after
-   write(*,*) "SYMBA_FRAG_POS KE_ratio  : ", (KE_after + KE_spin_after) / (KE_before + KE_spin_before)
-   write(*,*) "SYMBA_FRAG_POS U_before  : ", U_before
-   write(*,*) "SYMBA_FRAG_POS U_after   : ", U_after
-   write(*,*) "SYMBA_FRAG_POS U_ratio   : ", U_after / U_before
-   write(*,*) "SYMBA_FRAG_POS E_before  : ", KE_before + KE_spin_before + U_before
-   write(*,*) "SYMBA_FRAG_POS E_after   : ", KE_after + KE_spin_after + U_after
-   write(*,*) "SYMBA_FRAG_POS E_ratio   : ", (KE_after + KE_spin_after + U_after) / (KE_before + KE_spin_before + U_before)
+      ! Calculate the position of each fragment 
+      ! Theta is a phase shift value that ensures that successive nearby collisions in a single step are rotated to avoid possible overlap
+      theta = (2 * PI) / nfrag
+      ! Shifts the starting circle of fragments around so that multiple fragments generated 
+      ! from a single collision in a single time step don't pile up on top of each other
+      phase_ang = theta * thetashift / SHIFTMAX
+      thetashift = thetashift + 1
+      IF (thetashift >= shiftmax) thetashift = 0
 
-   do i = 1, nfrag
-      x_frag(:, i) = x_frag(:, i) + xcom(:)
-      v_frag(:, i) = v_frag(:, i) + vcom(:)
-   end do
+      ! Calculate the triple product to get the plane of the fragment distribution
+      delta_v(:) = v(:, 2) - v(:, 1)
+      delta_x(:) = x(:, 2) - x(:, 1)
+      call util_crossproduct(delta_v,delta_x,v_cross_x)
+      call util_crossproduct(v_cross_x,delta_v,tri_pro)
+      tri_pro_unit_vec(:) = tri_pro(:) / norm2(tri_pro(:))
+      v_col_norm = norm2(delta_v(:))               ! pre-collision velocity magnitude
+      r_col_norm = norm2(delta_x(:))               ! pre-collision distance 
+      v_col_unit_vec(:) = delta_v(:) / v_col_norm  ! unit vector of collision velocity 
 
-   deallocate(family)
+      do i = 1, nfrag
+         ! Place the fragments on the collision plane at a distance proportional to mass wrt the collisional barycenter
+         ! This gets updated later after the new potential energy is calculated
+         r_frag_norm = r_col_norm * mtot / m_frag(i)
+
+         x_frag(:,i) =  r_frag_norm * ((cos(phase_ang + theta * i)) * v_col_unit_vec(:)  + &
+                                       (sin(phase_ang + theta * i)) * tri_pro_unit_vec(:)) 
+                        
+         ! Apply a simple mass weighting first to ensure that the velocity follows the barycenter
+         ! This gets updated later after the new potential and kinetic energy is calcualted
+         v_frag_norm = v_col_norm * mtot / m_frag(i)   
+         v_frag(:,i) =  v_frag_norm * ((cos(phase_ang + theta * i)) * v_col_unit_vec(:) + &
+                                       (sin(phase_ang + theta * i)) * tri_pro_unit_vec(:)) 
+      end do
+      ! Adjust fragment positionx so that their center of mass follows the original pre-impact trajectory
+      call symba_frag_adjust (xcom, vcom, x, v, mass, radius, L_spin, Ip_frag, m_frag, rad_frag, x_frag, v_frag, rot_frag)
+
+      do i = 1, nfrag
+         x_frag(:, i) = x_frag(:, i) + xcom(:)
+         v_frag(:, i) = v_frag(:, i) + vcom(:)
+      end do
+
+      U_after = 0.0_DP
+      ! Calculate the new potential energy of the system 
+      do i = 1, nfrag
+         ! Calculate the contribution of the potential energy between fragments
+         do j = i+1,nfrag
+            U_after = U_after - m_frag(i) * m_frag(j) / norm2(x_frag(:,i) - x_frag(:,j))
+         end do
+         ! Add the contribution due to non-family members
+         do j = 1, non_fam_size
+            U_after = U_after - m_frag(i) * Mpl(non_family(j)) / norm2(x_frag(:,i) - xhpl(:,non_family(j)))
+         end do
+      end do
+
+      ! Compute the current kinetic energy of the system so we can scale the velocity vectors to the correct value
+      KE_after = 0.0_DP
+      KE_spin_after = 0.0_DP
+      do i = 1, nfrag
+         KE_after = KE_after + 0.5_DP * m_frag(i) * dot_product(v_frag(:,i), v_frag(:,i))
+         KE_spin_after = 0.5_DP * m_frag(i) * rad_frag(i)**2 * Ip_frag(3,i) * dot_product(rot_frag(:,i), rot_frag(:,i))
+      end do
+
+      ! Adjust the fragment velocities so that they have the their total energy reduced by an amount set by the anelastic parameter
+      ! Make sure we don't end up with negative energy (bound system). If so, we'll adjust the radius so that the potential energy
+      ! takes up the negative part
+      f_anelastic = 0.1_DP ! TODO: Should this be set by the user or kept as a constant?
+      KE_corrected = f_anelastic * Etot_before - KE_spin_after - U_after
+
+      v_frag(:,:) = v_frag(:,:) * sqrt(KE_corrected / KE_after) 
+
+      ! REMOVE THE FOLLOWING AFTER TESTING
+      ! Calculate the new energy of the system of fragments
+      KE_after = 0.0_DP
+      do i = 1, nfrag
+         KE_after = KE_after + 0.5_DP * m_frag(i) * dot_product(v_frag(:,i), v_frag(:,i))
+      end do
+
+      write(*,*) "SYMBA_FRAG_POS KE_before : ", KE_before + KE_spin_before
+      write(*,*) "SYMBA_FRAG_POS KE_after  : ", KE_after + KE_spin_after
+      write(*,*) "SYMBA_FRAG_POS KE_ratio  : ", (KE_after + KE_spin_after) / (KE_before + KE_spin_before)
+      write(*,*) "SYMBA_FRAG_POS U_before  : ", U_before
+      write(*,*) "SYMBA_FRAG_POS U_after   : ", U_after
+      write(*,*) "SYMBA_FRAG_POS U_ratio   : ", U_after / U_before
+      write(*,*) "SYMBA_FRAG_POS E_before  : ", KE_before + KE_spin_before + U_before
+      write(*,*) "SYMBA_FRAG_POS E_after   : ", KE_after + KE_spin_after + U_after
+      write(*,*) "SYMBA_FRAG_POS E_ratio   : ", (KE_after + KE_spin_after + U_after) / (KE_before + KE_spin_before + U_before)
+
+      deallocate(family, non_family)
+   end associate
    return 
 end subroutine symba_frag_pos
