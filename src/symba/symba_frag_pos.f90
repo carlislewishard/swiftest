30c896de8f38bff20645ae21a6f45c224f2bf701
David Minton <daminton@purdue.edu>
2021-04-08 09:38:09 -0400

    Reverted fragment radial position initialization to one based on mean progenitor position

--- src/symba/symba_frag_pos.f90
+++ src/symba/symba_frag_pos.f90
@@ -88,7 +88,7 @@ subroutine symba_frag_pos (symba_plA, idx_parents, x, v, L_spin, Ip, mass, radiu
          Ltot(:) = Ltot(:) + Mpl(family(i)) * (h(:) + Ippl(3, family(i)) * radpl(family(i))**2 * rotpl(:,family(i)))
          KE_before = KE_before + 0.5_DP * Mpl(family(i)) * v2 
          KE_spin_before = KE_spin_before + 0.5_DP * Mpl(family(i)) * Ippl(3,family(i)) * rot2 * radpl(family(i))**2
-         r_col_norm = r_col_norm + radpl(family(i)) !Mpl(family(i)) * norm2(xbpl(:,family(i)) - xcom(:))
+         r_col_norm = r_col_norm + Mpl(family(i)) * norm2(xbpl(:,family(i)) - xcom(:))
       end do
 
       Etot_before = KE_before + KE_spin_before + U_before
@@ -114,12 +114,11 @@ subroutine symba_frag_pos (symba_plA, idx_parents, x, v, L_spin, Ip, mass, radiu
       tri_pro_unit_vec(:) = tri_pro(:) / norm2(tri_pro(:))
       v_col_norm = norm2(delta_v(:))               ! pre-collision velocity magnitude
       v_col_unit_vec(:) = delta_v(:) / v_col_norm  ! unit vector of collision velocity 
-      !r_col_norm = r_col_norm / nfrag
-      r_frag_norm = 2 * rad_frag(1) / theta
+      r_col_norm = r_col_norm / mtot / nfrag
       do i = 1, nfrag
          ! Place the fragments on the collision plane at a distance proportional to mass wrt the collisional barycenter
          ! This gets updated later after the new potential energy is calculated
-         !r_frag_norm = r_col_norm * mtot / m_frag(i)
+         r_frag_norm = r_col_norm * mtot / m_frag(i)
 
          x_frag(:,i) =  r_frag_norm * ((cos(phase_ang + theta * i)) * v_col_unit_vec(:)  + &
                                        (sin(phase_ang + theta * i)) * tri_pro_unit_vec(:)) 
