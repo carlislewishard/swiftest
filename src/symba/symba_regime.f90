4c2cce59af906efd8c52658345be8157462fe039
David Minton <daminton@purdue.edu>
2021-04-09 14:38:29 -0400

    Added c_star interpolation function and refactored variable names to better match source equations.

--- src/symba/symba_regime.f90
+++ src/symba/symba_regime.f90
@@ -1,4 +1,4 @@
-subroutine symba_regime(Mcb, m1, m2, rad1, rad2, xh1, xh2, vb1, vb2, den1, den2, regime, mlr, mslr, mtiny)
+subroutine symba_regime(Mcb, m1, m2, rad1, rad2, xh1, xh2, vb1, vb2, den1, den2, regime, Mlr, Mslr, mtiny)
    !! Author: Jennifer L.L. Pouplin, Carlisle A. Wishard, and David A. Minton
    !!
    !! Determine the collisional regime of two colliding bodies. 
@@ -10,6 +10,8 @@ subroutine symba_regime(Mcb, m1, m2, rad1, rad2, xh1, xh2, vb1, vb2, den1, den2,
    !!          a realistic collision prescription. Mon Not R Astron Soc 478, 2896–2908. https://doi.org/10.1093/mnras/sty1273
    !!       Rufu, R., Aharonson, O., 2019. Impact Dynamics of Moons Within a Planetary Potential. J. Geophys. Res. Planets 124, 
    !!          1008–1019. https://doi.org/10.1029/2018JE005798
+   !!      Stewart, S.T., Leinhardt, Z.M., 2012. Collisions between Gravity-dominated Bodies. II. The Diversity of Impact 
+   !!          Outcomes during the End Stage of Planet Formation. ApJ 751, 32. https://doi.org/10.1088/0004-637X/751/1/32
    !!
    use swiftest
    use module_symba
@@ -21,14 +23,13 @@ subroutine symba_regime(Mcb, m1, m2, rad1, rad2, xh1, xh2, vb1, vb2, den1, den2,
 
 ! Arguments
    integer(I4B), intent(out)         :: regime
-   real(DP), intent(out)          :: mlr, mslr
+   real(DP), intent(out)          :: Mlr, Mslr
    real(DP), intent(in)           :: Mcb, m1, m2, rad1, rad2, den1, den2, mtiny 
    real(DP), dimension(:), intent(in)   :: xh1, xh2, vb1, vb2
 ! Constants
    integer(I4B), parameter :: N1 = 1  !number of objects with mass equal to the largest remnant from LS12
    integer(I4B), parameter :: N2 = 2  !number of objects with mass larger than second largest remnant from LS12
    real(DP), parameter   :: DENSITY1 = 1000.0_DP !standard density parameter from LS12 [kg/m3]
-   real(DP), parameter   :: C_STAR = 1.8_DP !3.0 #3.0# #5#1.8 #1.8 #measure of dissipation of energy within the target (chambers frag.f90)
    real(DP), parameter   :: MU_BAR = 0.37_DP !0.385#0.37#0.3333# 3.978 # 1/3 material parameter for hydrodynamic planet-size bodies (LS12)
    real(DP), parameter   :: BETA = 2.85_DP !slope of sfd for remnants from LS12 2.85
    real(DP), parameter   :: C1 = 2.43_DP !SL12 constants (see equation 15)
@@ -39,112 +40,113 @@ subroutine symba_regime(Mcb, m1, m2, rad1, rad2, xh1, xh2, vb1, vb2, den1, den2,
    real(DP), parameter   :: CRUFU = 2.0_DP - 3 * MU_BAR ! central potential variable from Rufu and Aharonson (2019)
    real(DP), parameter   :: SUPERCAT_QRATIO = 1.8_DP ! See Section 4.1 of LS12
 ! Internals
-   real(DP)           :: a1, alpha, aint, b, bcrit, egy, fgamma, l, lint, mu, phi, theta
+   real(DP)           :: a1, alpha, aint, b, bcrit, c_star, egy, zeta, l, lint, mu, phi, theta
    real(DP)           :: Qr, Qrd_pstar, Qr_erosion, Qr_supercat
-   real(DP)           :: vcr, verosion, vescp, vhill, vimp, vsupercat
-   real(DP)           :: mint, mtot
-   real(DP)           :: rp, rhill 
-   real(DP)           :: mresidual
+   real(DP)           :: Vhr, Verosion, Vescp, Vhill, Vimp, Vsupercat
+   real(DP)           :: Mint, Mtot
+   real(DP)           :: Rp, rhill 
+   real(DP)           :: Mresidual
 
 ! Executable code
-   vimp = norm2(vb2(:) - vb1(:))
+   Vimp = norm2(vb2(:) - vb1(:))
    b = calc_b(xh2, vb2, xh1, vb1)
    l = (rad1 + rad2) * (1 - b)
    egy = norm2(vb1)**2 / 2.0_DP - GC * Mcb / norm2(xh1)
    a1 = - GC * Mcb / 2.0_DP / egy
-   mtot = m1 + m2 
-   mu = (m1 * m2) / mtot
+   Mtot = m1 + m2 
+   mu = (m1 * m2) / Mtot
    if (l < 2 * rad2) then
-      !calculate mint
+      !calculate Mint
       phi = 2 * acos((l - rad2) / rad2)
       aint = rad2**2 * (PI - (phi - sin(phi)) / 2.0_DP)
       lint = 2 * sqrt(rad2**2 - (rad2 - l / 2.0_DP) ** 2) 
-      mint = aint * lint  ![kg]
+      Mint = aint * lint  ![kg]
       alpha = (l**2) * (3 * rad2 - l) / (4 * (rad2**3))
    else
       alpha = 1.0_DP
-      mint = m2
+      Mint = m2
    end if 
-   rp = (3 * (m1 / den1 + alpha * m2 / den2) / (4 * PI))**(1.0_DP/3.0_DP) ! (Mustill et al. 2018)
-   !calculate vescp
-   vescp = sqrt(2 * GC * mtot / rp) !Mustill et al. 2018 eq 6 
+   Rp = (3 * (m1 / den1 + alpha * m2 / den2) / (4 * PI))**(1.0_DP/3.0_DP) ! (Mustill et al. 2018)
+   c_star = calc_c_star(Rp)
+   !calculate Vescp
+   Vescp = sqrt(2 * GC * Mtot / Rp) !Mustill et al. 2018 eq 6 
    !calculate rhill
    rhill = a1 * (m1 / 3.0_DP / (Mcb + m1))**(1.0_DP/3.0_DP)
-   !calculate vhill
+   !calculate Vhill
    if ((rad2 + rad1) < rhill) then 
-     vhill = sqrt(2 * GC * m1 * ((rhill**2 - rhill * (rad1 + rad2)) / &
+     Vhill = sqrt(2 * GC * m1 * ((rhill**2 - rhill * (rad1 + rad2)) / &
      (rhill**2 - 0.5_DP * (rad1 + rad2)**2)) / (rad1 + rad2))
    else
-     vhill = vescp
+     Vhill = Vescp
    end if 
    !calculate Qr_pstar
-   Qrd_pstar = calc_Qrd_pstar(m1, m2, alpha) * (vhill / vescp)**CRUFU !Rufu and Aharaonson eq (3)
-   !calculate verosion
-   Qr_erosion = 2 * (1.0_DP - m1 / mtot) * Qrd_pstar
-   verosion = (2 * Qr_erosion * mtot / mu)** (1.0_DP / 2.0_DP)
-   Qr = mu*(vimp**2) / mtot / 2.0_DP
-   !calculate mass largest remnant mlr 
-   mlr = (1.0_DP - Qr / Qrd_pstar / 2.0_DP) * mtot  ! [kg] # LS12 eq (5)
-   !calculate vsupercat
+   Qrd_pstar = calc_Qrd_pstar(m1, m2, alpha, c_star) * (Vhill / Vescp)**CRUFU !Rufu and Aharaonson eq (3)
+   !calculate Verosion
+   Qr_erosion = 2 * (1.0_DP - m1 / Mtot) * Qrd_pstar
+   Verosion = (2 * Qr_erosion * Mtot / mu)** (1.0_DP / 2.0_DP)
+   Qr = mu*(Vimp**2) / Mtot / 2.0_DP
+   !calculate mass largest remnant Mlr 
+   Mlr = (1.0_DP - Qr / Qrd_pstar / 2.0_DP) * Mtot  ! [kg] # LS12 eq (5)
+   !calculate Vsupercat
    Qr_supercat = SUPERCAT_QRATIO * Qrd_pstar ! See LS12 Section 4.1 
-   vsupercat = sqrt(2 * Qr_supercat * mtot / mu)
-   !calculate vcr
-   fgamma = (m1 - m2) / mtot
+   Vsupercat = sqrt(2 * Qr_supercat * Mtot / mu)
+   !calculate Vhr
+   zeta = (m1 - m2) / Mtot
    theta = 1.0_DP - b
-   vcr = vescp * (C1 * fgamma**2 * theta**C5 + C2 * fgamma**2 + C3 * theta**C5 + C4) ! See SL12 equation 15
+   Vhr = Vescp * (C1 * zeta**2 * theta**C5 + C2 * zeta**2 + C3 * theta**C5 + C4) ! See SL12 equation 15
    bcrit = rad1 / (rad1 + rad2)
 
    if ((m1 < mtiny).or.(m2 < mtiny)) then 
      regime = COLLRESOLVE_REGIME_MERGE !perfect merging regime
-     mlr = mtot
-     mslr = 0.0_DP
+     Mlr = Mtot
+     Mslr = 0.0_DP
      write(*,*) "FORCE MERGE"
    else 
-      if( vimp < vescp) then
+      if( Vimp < Vescp) then
          regime = COLLRESOLVE_REGIME_MERGE !perfect merging regime
-         mlr = mtot
-         mslr = 0.0_DP
-      else if (vimp < verosion) then 
+         Mlr = Mtot
+         Mslr = 0.0_DP
+      else if (Vimp < Verosion) then 
          if (b < bcrit) then
             regime = COLLRESOLVE_REGIME_MERGE !partial accretion regime"
-            mlr = mtot
-            mslr = 0.0_DP
-         else if ((b > bcrit) .and. (vimp < vcr)) then
+            Mlr = Mtot
+            Mslr = 0.0_DP
+         else if ((b > bcrit) .and. (Vimp < Vhr)) then
             regime = COLLRESOLVE_REGIME_MERGE ! graze and merge
-            mlr = mtot
-            mslr = 0.0_DP
+            Mlr = Mtot
+            Mslr = 0.0_DP
          else
-            mlr = m1
-            mslr = calc_Qrd_rev(m2,m1,mint,den1,den2,vimp)
+            Mlr = m1
+            Mslr = calc_Qrd_rev(m2, m1, Mint, den1, den2, Vimp, c_star)
             regime = COLLRESOLVE_REGIME_HIT_AND_RUN !hit and run
          end if 
-      else if (vimp > verosion .and. vimp < vsupercat) then
+      else if (Vimp > Verosion .and. Vimp < Vsupercat) then
          if (m2 < 0.001_DP * m1) then 
             regime = COLLRESOLVE_REGIME_MERGE !cratering regime"
-            mlr = mtot
-            mslr = 0.0_DP
+            Mlr = Mtot
+            Mslr = 0.0_DP
          else 
-            mslr = mtot * (3.0_DP - BETA) * (1.0_DP - N1 * mlr / mtot) / (N2 * BETA)  ! LS12 eq (37)
+            Mslr = Mtot * (3.0_DP - BETA) * (1.0_DP - N1 * Mlr / Mtot) / (N2 * BETA)  ! LS12 eq (37)
             regime = COLLRESOLVE_REGIME_DISRUPTION !disruption
          end if 
-      else if (vimp > vsupercat) then 
-         mlr = mtot * 0.1_DP * (Qr / (Qrd_pstar * SUPERCAT_QRATIO))**(-1.5_DP)   !LS12 eq (44)
-         mslr = mtot * (3.0_DP - BETA) * (1.0_DP - N1 * mlr / mtot) / (N2 * BETA)  !LS12 eq (37)
+      else if (Vimp > Vsupercat) then 
+         Mlr = Mtot * 0.1_DP * (Qr / (Qrd_pstar * SUPERCAT_QRATIO))**(-1.5_DP)   !LS12 eq (44)
+         Mslr = Mtot * (3.0_DP - BETA) * (1.0_DP - N1 * Mlr / Mtot) / (N2 * BETA)  !LS12 eq (37)
          regime = COLLRESOLVE_REGIME_SUPERCATASTROPHIC ! supercatastrophic
       else 
          write(*,*) "Error no regime found in symba_regime"
       end if 
    end if 
-   mresidual = mtot - mlr - mslr
-   if (mresidual < 0.0_DP) then ! prevents final masses from going negative
-      mlr = mlr + mresidual
+   Mresidual = Mtot - Mlr - Mslr
+   if (Mresidual < 0.0_DP) then ! prevents final masses from going negative
+      Mlr = Mlr + Mresidual
    end if
       
    return 
 
 ! Internal functions
 contains
-   function calc_Qrd_pstar(mtarg,mp,alpha) result(Qrd_pstar)
+   function calc_Qrd_pstar(Mtarg, Mp, alpha, c_star) result(Qrd_pstar)
       !! author: Jennifer L.L. Pouplin and Carlisle A. Wishard
       !!
       !! Calculates the corrected Q* for oblique impacts. See Eq. (15) of LS12.
@@ -154,87 +156,113 @@ contains
       !! 
       implicit none
       ! Arguments
-      real(DP),intent(in) :: mtarg, mp, alpha
+      real(DP),intent(in) :: Mtarg, Mp, alpha, c_star
       ! Result
       real(DP)      :: Qrd_pstar
       ! Internals
       real(DP)      :: Qrd_star1, mu_alpha, mu, Qrd_star
 
       ! calc mu, mu_alpha
-      mu = (mtarg * mp) / (mtarg + mp)  ! [kg]
-      mu_alpha = (mtarg * alpha * mp) / (mtarg + alpha * mp)  ! [kg]
+      mu = (Mtarg * Mp) / (Mtarg + Mp)  ! [kg]
+      mu_alpha = (Mtarg * alpha * Mp) / (Mtarg + alpha * Mp)  ! [kg]
       ! calc Qrd_star1
-      Qrd_star1 = (C_STAR * 4 * PI * DENSITY1 * GC * rp**2) / 5.0_DP
+      Qrd_star1 = (c_star * 4 * PI * DENSITY1 * GC * Rp**2) / 5.0_DP
       ! calc Qrd_star
-      Qrd_star = Qrd_star1 * (((mp / mtarg + 1.0_DP)**2) / (4 * mp / mtarg))**(2.0_DP / (3.0_DP * MU_BAR) - 1.0_DP)  !(eq 23)
+      Qrd_star = Qrd_star1 * (((Mp / Mtarg + 1.0_DP)**2) / (4 * Mp / Mtarg))**(2.0_DP / (3.0_DP * MU_BAR) - 1.0_DP)  !(eq 23)
       ! calc Qrd_pstar, v_pstar
       Qrd_pstar = ((mu / mu_alpha)**(2.0_DP - 3.0_DP * MU_BAR / 2.0_DP)) * Qrd_star  ! (eq 15)
 
       return
    end function calc_Qrd_pstar
 
-   function calc_Qrd_rev(mp,mtarg,mint,den1,den2, vimp) result(mslr)
+   function calc_Qrd_rev(Mp, Mtarg, Mint, den1, den2, Vimp, c_star) result(Mslr)
       !! author: Jennifer L.L. Pouplin and Carlisle A. Wishard
       !!
       !! Calculates mass of second largest fragment.
       !! 
       implicit none
       ! Arguments
-      real(DP),intent(in) :: mp, mtarg, mint, den1, den2, vimp
+      real(DP),intent(in) :: Mp, Mtarg, Mint, den1, den2, Vimp, c_star
       ! Result
-      real(DP) :: mslr
+      real(DP) :: Mslr
       ! Internals
       real(DP) :: mtot_rev, mu_rev, gamma_rev, Qrd_star1, Qrd_star, mu_alpha_rev
-      real(DP) :: Qrd_pstar, rC1, Qr_rev, Qrd_pstar_rev, Qr_supercat_rev
+      real(DP) :: Qrd_pstar, Rc1, Qr_rev, Qrd_pstar_rev, Qr_supercat_rev
 
-      ! calc mtlr, rC1, mu, gammalr
-      mtot_rev =  mint + mp
-      rC1 = (3 * (mint / den1 + mp / den2) / (4 * PI))**(1.0_DP/3.0_DP) ! [m] Mustill et al 2018
-      mu_rev = (mint * mp) / mtot_rev ! [kg] eq 49 LS12
-      mu_alpha_rev = (mtarg * alpha * mp) / (mtarg + alpha * mp)
-      gamma_rev = mint / mp ! eq 50 LS12
+      ! calc Mslr, Rc1, mu, gammalr
+      mtot_rev =  Mint + Mp
+      Rc1 = (3 * (Mint / den1 + Mp / den2) / (4 * PI))**(1.0_DP/3.0_DP) ! [m] Mustill et al 2018
+      mu_rev = (Mint * Mp) / mtot_rev ! [kg] eq 49 LS12
+      mu_alpha_rev = (Mtarg * alpha * Mp) / (Mtarg + alpha * Mp)
+      gamma_rev = Mint / Mp ! eq 50 LS12
       !calc Qr_rev
-      Qr_rev = mu_rev * (vimp**2) / (2 * mtot_rev)
+      Qr_rev = mu_rev * (Vimp**2) / (2 * mtot_rev)
       ! calc Qrd_star1, v_star1
-      Qrd_star1 = (C_STAR * 4 * PI * mtot_rev * GC ) / rC1 / 5.0_DP
+      Qrd_star1 = (c_star * 4 * PI * mtot_rev * GC ) / Rc1 / 5.0_DP
       ! calc Qrd_pstar_rev
       Qrd_star = Qrd_star1 * (((gamma_rev + 1.0_DP)**2) / (4 * gamma_rev)) ** (2.0_DP / (3.0_DP * MU_BAR) - 1.0_DP) !(eq 52)
       Qrd_pstar = Qrd_star * ((mu_rev / mu_alpha_rev)**(2.0_DP - 3.0_DP * MU_BAR / 2.0_DP))
-      Qrd_pstar_rev = Qrd_pstar * (vhill / vescp)**CRUFU !Rufu and Aharaonson eq (3)
+      Qrd_pstar_rev = Qrd_pstar * (Vhill / Vescp)**CRUFU !Rufu and Aharaonson eq (3)
       !calc Qr_supercat_rev
       Qr_supercat_rev = 1.8_DP * Qrd_pstar_rev 
       if (Qr_rev > Qr_supercat_rev ) then 
-         mslr = mtot_rev * (0.1_DP * ((Qr_rev / (Qrd_pstar_rev * 1.8_DP))**(-1.5_DP)))   !eq (44)
+         Mslr = mtot_rev * (0.1_DP * ((Qr_rev / (Qrd_pstar_rev * 1.8_DP))**(-1.5_DP)))   !eq (44)
       else if ( Qr_rev < Qrd_pstar_rev ) then 
-         mslr = mp 
+         Mslr = Mp 
       else 
-         mslr = (1.0_DP - Qr_rev / Qrd_pstar_rev / 2.0_DP) * (mtot_rev)  ! [kg] #(eq 5)
+         Mslr = (1.0_DP - Qr_rev / Qrd_pstar_rev / 2.0_DP) * (mtot_rev)  ! [kg] #(eq 5)
       end if 
 
-      if ( mslr > mp ) mslr = mp !check conservation of mass
+      if ( Mslr > Mp ) Mslr = Mp !check conservation of mass
 
       return
    end function calc_Qrd_rev
 
-function calc_b(proj_pos, proj_vel, targ_pos, targ_vel) result(sintheta)
-   !! author: Jennifer L.L. Pouplin, Carlisle A. Wishard, and David A. Minton
-   !!
-   !! Calculates the impact factor b = sin(theta), where theta is the angle between the relative velocity
-   !! and distance vectors of the target and projectile bodies. See Fig. 2 of Leinhardt and Stewart (2012)
-   !! 
-   implicit none
-   !! Arguments
-   real(DP), dimension(:), intent(in) :: proj_pos, proj_vel, targ_pos, targ_vel
-   !! Result
-   real(DP)             :: sintheta
-   !! Internals
-   real(DP), dimension(NDIM)     :: imp_vel, distance, x_cross_v      
+   function calc_b(proj_pos, proj_vel, targ_pos, targ_vel) result(sintheta)
+      !! author: Jennifer L.L. Pouplin, Carlisle A. Wishard, and David A. Minton
+      !!
+      !! Calculates the impact factor b = sin(theta), where theta is the angle between the relative velocity
+      !! and distance vectors of the target and projectile bodies. See Fig. 2 of Leinhardt and Stewart (2012)
+      !! 
+      implicit none
+      !! Arguments
+      real(DP), dimension(:), intent(in) :: proj_pos, proj_vel, targ_pos, targ_vel
+      !! Result
+      real(DP)             :: sintheta
+      !! Internals
+      real(DP), dimension(NDIM)     :: imp_vel, distance, x_cross_v      
 
-   imp_vel(:) = proj_vel(:) - targ_vel(:)
-   distance(:) = proj_pos(:) - targ_pos(:)
-   call util_crossproduct(distance, imp_vel, x_cross_v)
-   sintheta = norm2(x_cross_v(:)) / norm2(distance(:)) / norm2(imp_vel(:))
-   return 
-end function calc_b
+      imp_vel(:) = proj_vel(:) - targ_vel(:)
+      distance(:) = proj_pos(:) - targ_pos(:)
+      call util_crossproduct(distance, imp_vel, x_cross_v)
+      sintheta = norm2(x_cross_v(:)) / norm2(distance(:)) / norm2(imp_vel(:))
+      return 
+   end function calc_b
 
+   function calc_c_star(Rc1) result(c_star)
+      !! author: David A. Minton
+      !!
+      !! Calculates c_star as a function of impact equivalent radius. It inteRpolates between 5 for ~1 km sized bodies to
+      !! 1.8 for ~10000 km sized bodies. See LS12 Fig. 4 for details.
+      !! 
+      implicit none
+      !! Arguments
+      real(DP), intent(in) :: Rc1
+      !! Result
+      real(DP)             :: c_star
+      !! Internals
+      real(DP), parameter  :: loR   = 1.0e3_DP ! Lower bound of inteRpolation size (m)
+      real(DP), parameter  :: hiR   = 1.0e7_DP ! Upper bound of inteRpolation size (m)
+      real(DP), parameter  :: loval = 5.0_DP   ! Value of C* at lower bound
+      real(DP), parameter  :: hival = 1.8_DP   ! Value of C* at upper bound
+
+      if (Rc1 < loR) then
+         c_star = loval
+      else if (Rc1 < hiR) then
+         c_star = loval + (hival - loval) * log(Rc1 / loR) / log(hiR /loR)
+      else
+         c_star = hival
+      end if
+      return
+   end function calc_c_star
 end subroutine symba_regime
