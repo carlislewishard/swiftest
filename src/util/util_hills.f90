fd59b5bcbdfb2be02f48fa35a4b7fee0b3507c69
David Minton <daminton@purdue.edu>
2021-04-22 12:58:12 -0400

    Updated Hill sphere calculation to handle hyperbolic orbits

--- src/util/util_hills.f90
+++ src/util/util_hills.f90
@@ -1,78 +1,44 @@
-!**********************************************************************************************************************************
-!
-!  Unit Name   : util_hills
-!  Unit Type   : subroutine
-!  Project     : Swiftest
-!  Package     : util
-!  Language    : Fortran 90/95
-!
-!  Description : Compute Hill sphere radii of planets
-!
-!  Input
-!    Arguments : npl          : number of planets
-!                swifter_pl1P : pointer to head of SWIFTER planet structure linked-list
-!    Terminal  : none
-!    File      : none
-!
-!  Output
-!    Arguments : swifter_pl1P : pointer to head of SWIFTER planet structure linked-list
-!    Terminal  : none
-!    File      : none
-!
-!  Invocation  : CALL util_hills(npl, swifter_pl1P)
-!
-!  Notes       : Adapted from Hal Levison's Swift routine util_hills.f
-!
-!**********************************************************************************************************************************
-SUBROUTINE util_hills(npl, swiftest_plA)
-
+subroutine util_hills(npl, swiftest_plA)
+   !! Author: David A. Minton
+   !!
+   !! CoGMpute Hill sphere radii of planet. in the case of hyperbolic orbits, use the heliocentric radius instead of semimajor axis
+   !!
+   !! Adapted from David E. Kaufmann's Swifter routine util_hills.f90
+   !! Adapted from Hal Levison's Swift routine util_hills.f
 ! Modules
-     USE swiftest
-     USE module_interfaces, EXCEPT_THIS_ONE => util_hills
-     IMPLICIT NONE
+   use swiftest
+   use module_interfaces, EXCEPT_THIS_ONE => util_hills
+   implicit none
 
-! Arguments
-     INTEGER(I4B), INTENT(IN)  :: npl
-     TYPE(swiftest_pl), INTENT(INOUT) :: swiftest_plA
+! arguments
+   integer(I4B), intent(in)  :: npl
+   type(swiftest_pl), intent(inout) :: swiftest_plA
 
-! Internals
-     INTEGER(I4B)              :: i
-     REAL(DP)                  :: msun, mp, mu, energy, ap, r, v2
+! internals
+   integer(I4B)        :: i
+   real(DP)            :: mu, energy, ap, r, v2
 
-! Executable code
-     msun = swiftest_plA%mass(1)
-     DO i = 2, npl
-          mp = swiftest_plA%mass(i)
-          IF (mp > 0.0_DP) THEN
-               mu = msun + mp
-               r = SQRT(DOT_PRODUCT(swiftest_plA%xh(:,i), swiftest_plA%xh(:,i)))
-               v2 = DOT_PRODUCT(swiftest_plA%vh(:,i), swiftest_plA%vh(:,i))
-               energy = 0.5_DP*v2 - mu/r
-               ap = -0.5_DP*mu/energy
-               swiftest_plA%rhill(i) = ap*(((mp/mu)/3.0_DP)**(1.0_DP/3.0_DP))
-          ELSE
-               swiftest_plA%rhill(i) = 0.0_DP
-          END IF
-     END DO
+! executable code
+   associate(GMcb => swiftest_plA%mass(1), GMp => swiftest_plA%mass, xh => swiftest_plA%xh, vh => swiftest_plA%vh, rhill => swiftest_plA%rhill)
+      do i = 2, npl
+         if (GMp(i) > 0.0_DP) then
+            mu = GMcb + GMp(i)
+            r = norm2(xh(:, i))
+            v2 = dot_product(vh(:, i), vh(:, i))
+            energy = 0.5_DP * v2 - mu / r
+            if (energy < 0.0_DP) then
+               ap = -0.5_DP * mu / energy
+            else
+               ap = r ! use the heliocentric radius for the hill radius of hyperbolic orbits 
+                     !(this is probably good enough for most purposes, but probably worth investigating)
+            end if
+            rhill(i) = ap * (((GMp(i) / mu) / 3.0_DP)**(1.0_DP / 3.0_DP))
+         else
+            rhill(i) = 0.0_DP
+         end if
+      end do
+   end associate
 
-     RETURN
+   return
 
-END SUBROUTINE util_hills
-!**********************************************************************************************************************************
-!
-!  Author(s)   : David E. Kaufmann
-!
-!  Revision Control System (RCS) Information
-!
-!  Source File : $RCSfile$
-!  Full Path   : $Source$
-!  Revision    : $Revision$
-!  Date        : $Date$
-!  Programmer  : $Author$
-!  Locked By   : $Locker$
-!  State       : $State$
-!
-!  Modification History:
-!
-!  $Log$
-!**********************************************************************************************************************************
+end subroutine util_hills
