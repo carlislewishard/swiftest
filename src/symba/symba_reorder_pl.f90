0a058a8010af844152c01db0e8242a568a0177c8
David Minton <daminton@purdue.edu>
2021-04-26 14:00:57 -0400

    Fixed bad reordering

--- src/symba/symba_reorder_pl.f90
+++ src/symba/symba_reorder_pl.f90
@@ -22,23 +22,34 @@ subroutine symba_reorder_pl(npl, symba_plA)
    allocate(sort_index(npl))
 
    ! sort by mass
-   call util_index(symba_plA%helio%swiftest%mass(2:npl), sort_index(2:npl))
+   call util_index(symba_plA%helio%swiftest%mass(1:npl), sort_index)
    !write(*,*) "************ REORDER ***************"
-   do i = 2, npl
-      symba_plA%helio%swiftest%name(i) = symba_plwkspA%helio%swiftest%name(sort_index(npl-i+2))
-      symba_plA%helio%swiftest%status(i) = symba_plwkspA%helio%swiftest%status(sort_index(npl-i+2))
-      symba_plA%helio%swiftest%mass(i) = symba_plwkspA%helio%swiftest%mass(sort_index(npl-i+2))
-      symba_plA%helio%swiftest%radius(i) = symba_plwkspA%helio%swiftest%radius(sort_index(npl-i+2))
-      symba_plA%helio%swiftest%xh(:,i) = symba_plwkspA%helio%swiftest%xh(:,sort_index(npl-i+2))
-      symba_plA%helio%swiftest%vh(:,i) = symba_plwkspA%helio%swiftest%vh(:,sort_index(npl-i+2))
-      symba_plA%helio%swiftest%xb(:,i) = symba_plwkspA%helio%swiftest%xb(:,sort_index(npl-i+2))
-      symba_plA%helio%swiftest%vb(:,i) = symba_plwkspA%helio%swiftest%vb(:,sort_index(npl-i+2))
-      symba_plA%helio%swiftest%rot(:,i) = symba_plwkspA%helio%swiftest%rot(:,sort_index(npl-i+2))
-      symba_plA%helio%swiftest%Ip(:,i) = symba_plwkspA%helio%swiftest%Ip(:,sort_index(npl-i+2))
-      symba_plA%helio%swiftest%rhill(i) = symba_plwkspA%helio%swiftest%rhill(sort_index(npl-i+2))
+   do i = 1, npl
+      symba_plwkspA%helio%swiftest%name(i) = symba_plA%helio%swiftest%name(sort_index(npl-i+1))
+      symba_plwkspA%helio%swiftest%status(i) = symba_plA%helio%swiftest%status(sort_index(npl-i+1))
+      symba_plwkspA%helio%swiftest%mass(i) = symba_plA%helio%swiftest%mass(sort_index(npl-i+1))
+      symba_plwkspA%helio%swiftest%radius(i) = symba_plA%helio%swiftest%radius(sort_index(npl-i+1))
+      symba_plwkspA%helio%swiftest%xh(:,i) = symba_plA%helio%swiftest%xh(:,sort_index(npl-i+1))
+      symba_plwkspA%helio%swiftest%vh(:,i) = symba_plA%helio%swiftest%vh(:,sort_index(npl-i+1))
+      symba_plwkspA%helio%swiftest%xb(:,i) = symba_plA%helio%swiftest%xb(:,sort_index(npl-i+1))
+      symba_plwkspA%helio%swiftest%vb(:,i) = symba_plA%helio%swiftest%vb(:,sort_index(npl-i+1))
+      symba_plwkspA%helio%swiftest%rot(:,i) = symba_plA%helio%swiftest%rot(:,sort_index(npl-i+1))
+      symba_plwkspA%helio%swiftest%Ip(:,i) = symba_plA%helio%swiftest%Ip(:,sort_index(npl-i+1))
+      symba_plwkspA%helio%swiftest%rhill(i) = symba_plA%helio%swiftest%rhill(sort_index(npl-i+1))
    end do
+   call move_alloc(symba_plwkspA%helio%swiftest%name, symba_plA%helio%swiftest%name)
+   call move_alloc(symba_plwkspA%helio%swiftest%status, symba_plA%helio%swiftest%status)
+   call move_alloc(symba_plwkspA%helio%swiftest%mass, symba_plA%helio%swiftest%mass)
+   call move_alloc(symba_plwkspA%helio%swiftest%radius, symba_plA%helio%swiftest%radius)
+   call move_alloc(symba_plwkspA%helio%swiftest%xh, symba_plA%helio%swiftest%xh)
+   call move_alloc(symba_plwkspA%helio%swiftest%vh, symba_plA%helio%swiftest%vh)
+   call move_alloc(symba_plwkspA%helio%swiftest%xb, symba_plA%helio%swiftest%xb)
+   call move_alloc(symba_plwkspA%helio%swiftest%vb, symba_plA%helio%swiftest%vb)
+   call move_alloc(symba_plwkspA%helio%swiftest%rot, symba_plA%helio%swiftest%rot)
+   call move_alloc(symba_plwkspA%helio%swiftest%Ip, symba_plA%helio%swiftest%Ip)
+   call move_alloc(symba_plwkspA%helio%swiftest%rhill, symba_plA%helio%swiftest%rhill)
    
-   narr = size(symba_plA%helio%swiftest%status(:))
+   narr = size(symba_plwkspA%helio%swiftest%status(:))
    symba_plA%helio%swiftest%name(npl+1:narr) = 0
    symba_plA%helio%swiftest%status(npl+1:narr) = INACTIVE
    call symba_pl_deallocate(symba_plwkspA)
