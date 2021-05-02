81c75c63351b75292eebfaa82bcfaef8d982f4e4
David Minton <daminton@purdue.edu>
2021-05-02 00:18:50 -0400

    Cleaned up discard code

--- src/symba/symba_discard_conserve_mtm.f90
+++ src/symba/symba_discard_conserve_mtm.f90
@@ -11,53 +11,38 @@ subroutine symba_discard_conserve_mtm(swiftest_plA, ipl, lescape)
    type(swiftest_pl), intent(inout) :: swiftest_plA
    logical, intent(in)         :: lescape
 
-   real(DP)            :: mass, rad, Ipz, Ipcbz, Mcb, radcb
-   real(DP), dimension(NDIM) :: Lpl, Lcb, rot, rotcb, xb, vb, xbcb, vbcb, xcom, vcom
+   real(DP), dimension(NDIM) :: Lpl, Lcb, xcom, vcom
 
-   xb(:) = swiftest_plA%xb(:,ipl)
-   vb(:) = swiftest_plA%vb(:,ipl)
-   rot(:) = swiftest_plA%rot(:,ipl)
-   Ipz = swiftest_plA%Ip(3,ipl)
-   rad = swiftest_plA%radius(ipl)
-   mass = swiftest_plA%mass(ipl) 
-
-   xbcb(:) = swiftest_plA%xb(:,1)
-   vbcb(:) = swiftest_plA%vb(:,1)
-   rotcb(:) = swiftest_plA%rot(:,1)
-   Ipcbz = swiftest_plA%Ip(3,1)
-   radcb = swiftest_plA%radius(1)
-   Mcb = swiftest_plA%mass(1)
-
-   xcom(:) = (mass * xb(:) + Mcb * xbcb(:)) / (mass + Mcb)
-   vcom(:) = (mass * vb(:) + Mcb * vbcb(:)) / (mass + Mcb)
-
-   call util_crossproduct(xb - xcom,vb - vcom, Lpl)
-   Lpl(:) = mass * (Lpl(:) + rad**2 * Ipz * rot(:))
-
-   call util_crossproduct(xbcb-xcom,vbcb-vcom,Lcb)
-   Lcb(:) = Mcb * Lcb(:) 
-
-   ! Add planet mass to central body accumulator
-   if (lescape) then
-      swiftest_plA%Mescape = swiftest_plA%Mescape + mass
-   else
-      swiftest_plA%dMcb = swiftest_plA%dMcb + mass
-
-      ! Update mass of central body to be consistent with its total mass
-      Mcb = swiftest_plA%Mcb_initial + swiftest_plA%dMcb
-      swiftest_plA%mass(1) = Mcb
-   end if
+   associate(xb => swiftest_plA%xb, vb => swiftest_plA%vb, rot => swiftest_plA%rot, Ip => swiftest_plA%Ip, rad => swiftest_plA%radius, mass => swiftest_plA%mass)
    
-   ! Add planet angular momentum to central body accumulator
-   swiftest_plA%dLcb(:) = Lpl(:) + Lcb(:) + swiftest_plA%dLcb(:)
+      xcom(:) = (mass(ipl) * xb(:, ipl) + mass(1) * xb(:,1)) / (mass(1) + mass(ipl))
+      xcom(:) = (mass(ipl) * vb(:, ipl) + mass(1) * vb(:,1)) / (mass(1) + mass(ipl))
 
-   ! Update rotation of central body to by consistent with its angular momentum 
-   swiftest_plA%rot(:,1) = (swiftest_plA%Lcb_initial(:) + swiftest_plA%dLcb(:)) / (Ipcbz * Mcb * radcb**2)        
+      call util_crossproduct(xb(:,ipl) - xcom(:),vb(:,ipl) - vcom(:), Lpl)
+      Lpl(:) = mass(ipl) * (Lpl(:) + rad(ipl)**2 * Ip(3,ipl) * rot(:, ipl))
+
+      call util_crossproduct(xb(:, 1) - xcom(:), vb(:, 1) - vcom(:), Lcb)
+      Lcb(:) = mass(1) * Lcb(:) 
+
+      ! Add planet mass to central body accumulator
+      if (lescape) then
+         swiftest_plA%Mescape = swiftest_plA%Mescape + mass(ipl)
+      else
+         swiftest_plA%dMcb = swiftest_plA%dMcb + mass(ipl)
+         ! Update mass of central body to be consistent with its total mass
+         mass(1) = swiftest_plA%Mcb_initial + swiftest_plA%dMcb
+      end if
+   
+      ! Add planet angular momentum to central body accumulator
+      swiftest_plA%dLcb(:) = Lpl(:) + Lcb(:) + swiftest_plA%dLcb(:)
+
+      ! Update rotation of central body to by consistent with its angular momentum 
+      swiftest_plA%rot(:,1) = (swiftest_plA%Lcb_initial(:) + swiftest_plA%dLcb(:)) / (Ip(3, 1) * mass(1) * rad(1)**2)        
       
-   ! Update position and velocity of central body
-   swiftest_plA%xb(:,1) = xcom(:)
-   swiftest_plA%vb(:,1) = vcom(:)
-
+      ! Update position and velocity of central body
+      xb(:, 1) = xcom(:)
+      vb(:, 1) = vcom(:)
+   end associate
    return
 
 end subroutine symba_discard_conserve_mtm
