7da0bf6193d7828d4d04d98ce503664d3318bf25
David Minton <daminton@purdue.edu>
2021-05-10 12:29:15 -0400

    Reduced fragment numbers back down to avoid blowing up the system with fragments

--- src/symba/symba_casesupercatastrophic.f90
+++ src/symba/symba_casesupercatastrophic.f90
@@ -32,7 +32,7 @@ function symba_casesupercatastrophic (symba_plA, family, nmergeadd, mergeadd_lis
    logical                                 :: lmerge
   
    ! Collisional fragments will be uniformly distributed around the pre-impact barycenter
-   nfrag = 42 ! This value is set for testing. This needs to be updated such that it is calculated or set by the user
+   nfrag = 20 ! This value is set for testing. This needs to be updated such that it is calculated or set by the user
    allocate(m_frag(nfrag))
    allocate(rad_frag(nfrag))
    allocate(xb_frag(NDIM, nfrag))
